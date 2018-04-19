e7cf9fc5f78531f100a7157da0184520f6662f7e
kleidemos <19260292+kleidemos@users.noreply.github.com>
2018-04-19 23:41:34 +0500

    Добавил перечеь параметров со значениями.

--- Rosdex.Parser/src/Rosdex.Parser/Scripts/Repl1.fsx
+++ Rosdex.Parser/src/Rosdex.Parser/Scripts/Repl1.fsx
@@ -87,9 +87,68 @@ products'
         else
             Some (p, category))
 
+regrouppedProducts
+    |> List.collect (fun p -> p.Data.Params)
+    |> List.map (fun p -> p.Name)
+    |> List.distinct
+    |> List.length
+
 open Rosdex.Parser.Csv
-open Microsoft.FSharpLu
 open Utils
+open Microsoft.FSharpLu
+
+type Counts = {
+    Value : string
+    Count : int
+}
+
+let values =
+    regrouppedProducts
+    |> List.collect (fun p -> p.Data.Params |> List.map (fun q -> q, p.CategoryId))
+    |> List.countBy id
+    |> List.map (fun ((param, catId), count) ->
+        catId, (param, count))
+    |> List.groupBy fst
+    |> List.map (fun (cId, p) ->
+        cId |> Catalog.find categories' |> fun p -> p.Name
+        , p
+            |> List.map (fun (_, (param, count)) -> param.Name, { Value = param.Value; Count = count})
+            |> List.groupBy fst
+            |> List.map (fun (name, p) -> name, List.map snd p)
+            |> Map.ofList
+        )
+    |> Map.ofList
+
+values
+    |> Map.toList
+    |> List.map (fun (catId, byNames) -> catId, byNames |> Map.toList)
+    |> List.collect (
+        let tab = sprintf "    %s"
+        let itemTab = sprintf "  - %s"
+        fun (name, names) ->
+            [
+                yield name
+                yield!
+                    names
+                    |> List.collect (fun (name, counts) ->
+                        [
+                            yield itemTab name
+                            yield!
+                                counts
+                                |> List.collect(fun p ->
+                                    [   p.Value.Replace("\n", "") |> itemTab
+                                        sprintf "%-3i шт" p.Count |> tab
+                                    ])
+                                |> List.map tab
+                        ])
+            ])
+    |> String.concat "\n"
+    |> fun p ->
+        System.IO.File.WriteAllText(
+            (Repl.gitignored.OutputDirectoryPath
+                |> Option.get
+                |> Repl.IO.Path.combine <| "values.tabyaml.txt")
+            , p)
 
 [
     yield
